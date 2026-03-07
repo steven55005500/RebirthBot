7b0d9c3d64ea0cf2d3b00bb0c940ba778eb88350
Steven Star <stevenstar5500@gmail.com>
2026-03-07 10:16:23 +0530

    update auto login stable watcher

--- loginSave.js
+++ loginSave.js
@@ -10,12 +10,19 @@ async function autoLogin(page) {
 
       console.log("Session expired → Logging in...");
 
+      await page.click("#txtusername",{clickCount:3});
+      await page.keyboard.press("Backspace");
+
       await page.type("#txtusername", process.env.LOGIN_ID, { delay: 50 });
+
+      await page.click("input[type=password]",{clickCount:3});
+      await page.keyboard.press("Backspace");
+
       await page.type("input[type=password]", process.env.LOGIN_PASS, { delay: 50 });
 
       await page.keyboard.press("Enter");
 
-      await page.waitForTimeout(6000);
+      await page.waitForTimeout(7000);
 
       console.log("LOGIN SUCCESS");
 
@@ -32,43 +39,65 @@ async function autoLogin(page) {
   }
 }
 
-(async () => {
+async function start() {
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    userDataDir: "./profile",
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage"
-    ]
-  });
+  try {
 
-  const page = await browser.newPage();
+    const browser = await puppeteer.launch({
+      headless: true,
+      userDataDir: "./profile",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu"
+      ]
+    });
 
-  await page.goto(
-    "https://www.rebirthcharity.com/Report/AutoPoolTeam",
-    { waitUntil: "networkidle2" }
-  );
+    const page = await browser.newPage();
 
-  await autoLogin(page);
+    page.setDefaultNavigationTimeout(0);
 
-  setInterval(async () => {
+    await page.goto(
+      "https://www.rebirthcharity.com/Report/AutoPoolTeam",
+      {
+        waitUntil: "domcontentloaded",
+        timeout: 0
+      }
+    );
 
-    try {
+    console.log("Watcher started");
 
-      await page.reload({ waitUntil: "networkidle2" });
+    await autoLogin(page);
 
-      await autoLogin(page);
+    setInterval(async () => {
 
-      console.log("Page refreshed & session checked");
+      try {
 
-    } catch (err) {
+        await page.reload({
+          waitUntil: "domcontentloaded",
+          timeout: 0
+        });
 
-      console.log("Reload error:", err.message);
+        await autoLogin(page);
 
-    }
+        console.log("Page refreshed & session checked");
 
-  }, 300000);
+      } catch (err) {
 
-})();
+        console.log("Reload error:", err.message);
+
+      }
+
+    }, 300000); // 5 minutes
+
+  } catch (err) {
+
+    console.log("Browser crash restarting...");
+
+    setTimeout(start,10000);
+
+  }
+}
+
+start();
